9bd184818feca7d5f7dbe19a9e89e1d37fb6bbad
Harshit khandelwal <live.harshit@hotmail.com>
2018-03-06 23:44:08 +0530

    List view added

--- app/src/main/java/com/liveharshit/android/miwok/Numbers.java
+++ app/src/main/java/com/liveharshit/android/miwok/Numbers.java
@@ -2,7 +2,9 @@ package com.liveharshit.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -26,13 +28,10 @@ public class Numbers extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
-        int index = 0;
-        for (index=0;index<words.size();index++) {
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(index));
-            rootView.addView(wordView);
-        }
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
     }
 }
